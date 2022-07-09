6786bc34e966083be1cb12751c6d788e4f5ce300
gimnathperera <gimnathperera@gmail.com>
2022-07-09 14:00:48 +0530

    [FEATURE] dynamic QR code generation

--- public/api/qr.js
+++ public/api/qr.js
@@ -1,6 +1,7 @@
-function generateQRCode() {
-  let book = document.getElementById('book').value;
-  console.log('>>===>> >>===>> book', book);
+const BASE_URL = 'http://localhost:8080';
+
+const generateQRCode = () => {
+  let book = document.getElementById('book-list').value;
 
   let qrcodeContainer = document.getElementById('qrcode');
   qrcodeContainer.innerHTML = '';
@@ -8,4 +9,27 @@ function generateQRCode() {
 
   document.getElementById('qrcode-base').style.display = 'flex';
   document.getElementById('qrcode-container').style.display = 'block';
-}
+};
+
+const fetchBookList = async () => {
+  try {
+    let response = await fetch(`${BASE_URL}/api/admin/book`);
+    let books = await response.json();
+
+    const bookList = books?.data
+      .map(
+        ({ bookName, id, author }) => `
+        <option value="${id}">${bookName} by ${author}</option>
+        `
+      )
+      .join('');
+
+    document
+      .querySelector('#book-list')
+      .insertAdjacentHTML('afterbegin', bookList);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+window.onload = fetchBookList;
